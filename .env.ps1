d5ce35b4d7ad13d317efaf1b4471c1766b3832d3
D. Lambert <dlambert@appdev.info>
2019-10-02 22:55:38 -0400

    Delete .env.ps1

--- .env.ps1
+++
@@ -1,5 +0,0 @@
-$env:ARM_CLIENT_ID='dd3b01e3-7edc-4f05-b984-a550ed7de59b'
-$env:ARM_CLIENT_SECRET='o:B][0So.keKjQbyzLdPPJVmMkrpi345'
-$env:ARM_SUBSCRIPTION_ID="2af46b25-b2c0-4b4c-b93e-b632e2f43b05"
-$env:ARM_TENANT_ID="51409593-bee1-4644-a95b-add824ce70a2"
-$env:TF_VAR_ARM_REGION="northcentralus"
